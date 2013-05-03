ab522362d6f65d6451bc40d1dc38c80672d15847
andris310 <andris310@gmail.com>
2013-05-02 19:04:45 -0700

    Memory game

---
+++ javascript.js
@@ -0,0 +1,82 @@
+
+$(document).ready(function() {
+
+	var letterArray = ['A','A','B','B','C','C','D','D'];
+  newArray = shuffle(letterArray);
+
+  function shuffle(o){ //v1.0
+    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+  };
+
+  var i = 0;
+  while(i < newArray.length){
+  	$('#t' + i).append($('<span>' + newArray[i] + '</span>'));
+  	$('span').addClass('hidden');
+  		i += 1;
+  }
+
+  $('#reset').on('click', function() {
+    $('span').removeClass('match');
+    $('span').addClass('hidden');
+    timeStart.call();
+    click.call();
+  });
+
+// ----------- timer -------------
+
+  function timeStart() {
+    var count = 0;
+    var counter = setInterval(timer, 1000);
+    function timer(){
+    count = count + 1;
+    if ($('span').hasClass('hidden') === false){
+       clearInterval(counter);
+       return;
+    }
+ document.getElementById("timer").innerHTML = count + " secs";
+  }
+}
+// ------------- timer -------------
+
+var click1;
+var click2;
+
+function click () {
+  $('td').on('click', function() {
+    $(this).children('span').removeClass('hidden');
+    $(this).children('span').addClass('active');
+    click1 = $(this).children('span');
+    $('td').off('click');
+    clickTwo.call();
+  });
+}
+
+function clickTwo () {
+  $('.hidden').parent().on('click', function() {
+
+    $(this).children('span').removeClass('hidden');
+    click2 = $(this).children('span');
+
+    if($(click1).text() != $(click2).text()){
+      setTimeout(function(){
+      $(click1).addClass('hidden');
+      $(click2).addClass('hidden');
+      }, 800);
+      $('td').off('click');
+      click.call();
+    } else if ($(click1).text() === $(click2).text()){
+        $(click1).addClass('match');
+        $(click2).addClass('match');
+        $('td').off('click');
+
+        if($('span').hasClass('hidden') === false){
+          $('td').off('click');
+          $('p').append($('<span>"YOU WIN!!!"</span>'));
+        } else {
+          click.call();
+      }}
+  });
+}
+
+});
