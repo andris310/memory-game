0b90f641553c10d0c4294fa61d6ed351c34b18d4
andris310 <andris310@gmail.com>
2013-05-02 22:56:57 -0700

    Added some css to the table

--- javascript.js
+++ javascript.js
@@ -5,15 +5,15 @@ $(document).ready(function() {
   newArray = shuffle(letterArray);
 
   function shuffle(o){ //v1.0
-    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    for( var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
-  };
+  }
 
   var i = 0;
   while(i < newArray.length){
   	$('#t' + i).append($('<span>' + newArray[i] + '</span>'));
   	$('span').addClass('hidden');
-  		i += 1;
+  	i += 1;
   }
 
   $('#reset').on('click', function() {
@@ -30,53 +30,58 @@ $(document).ready(function() {
     var counter = setInterval(timer, 1000);
     function timer(){
     count = count + 1;
-    if ($('span').hasClass('hidden') === false){
+
+    if(count > 10){
+      $('#timer').css('color', 'orange');
+       }
+
+    if($('span').hasClass('hidden') === false){
        clearInterval(counter);
        return;
     }
- document.getElementById("timer").innerHTML = count + " secs";
+   document.getElementById("timer").innerHTML = "Time: " + count;
+    }
   }
-}
-// ------------- timer -------------
+  // ------------- timer -------------
 
-var click1;
-var click2;
+  var click1;
+  var click2;
 
-function click () {
-  $('td').on('click', function() {
-    $(this).children('span').removeClass('hidden');
-    $(this).children('span').addClass('active');
-    click1 = $(this).children('span');
-    $('td').off('click');
-    clickTwo.call();
-  });
-}
-
-function clickTwo () {
-  $('.hidden').parent().on('click', function() {
-
-    $(this).children('span').removeClass('hidden');
-    click2 = $(this).children('span');
-
-    if($(click1).text() != $(click2).text()){
-      setTimeout(function(){
-      $(click1).addClass('hidden');
-      $(click2).addClass('hidden');
-      }, 800);
+  function click () {
+    $('td').on('click', function() {
+      $(this).children('span').removeClass('hidden');
+      $(this).children('span').addClass('active');
+      click1 = $(this).children('span');
       $('td').off('click');
-      click.call();
-    } else if ($(click1).text() === $(click2).text()){
-        $(click1).addClass('match');
-        $(click2).addClass('match');
-        $('td').off('click');
+      clickTwo.call();
+    });
+  }
 
-        if($('span').hasClass('hidden') === false){
+  function clickTwo () {
+    $('.hidden').parent().on('click', function() {
+
+      $(this).children('span').removeClass('hidden');
+      click2 = $(this).children('span');
+
+      if($(click1).text() != $(click2).text()){
+        setTimeout(function(){
+        $(click1).addClass('hidden');
+        $(click2).addClass('hidden');
+        }, 800);
+        $('td').off('click');
+        click.call();
+      } else if ($(click1).text() === $(click2).text()){
+          $(click1).addClass('match');
+          $(click2).addClass('match');
           $('td').off('click');
-          $('p').append($('<span>"YOU WIN!!!"</span>'));
-        } else {
-          click.call();
-      }}
-  });
-}
+
+          if($('span').hasClass('hidden') === false){
+            $('td').off('click');
+            $('#win').append($('<span>"YOU WIN!!!"</span>'));
+          } else {
+            click.call();
+        }}
+    });
+  }
 
 });
